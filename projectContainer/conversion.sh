e3c69a14624c2b64a2b85fdf0cb5a6affb4e93f3
Alex Bzdel <abzdel25@gmail.com>
2022-10-04 15:51:55 +0000

    building out conversion.sh, adding more files

--- projectContainer/conversion.sh
+++ projectContainer/conversion.sh
@@ -1,1 +1,8 @@
-echo "not implemented yet"
+echo "please enter your link to a csv data repository from Hugging Face"
+read line
+echo "your link is: $line"
+touch data.txt
+
+echo "$line" > data.txt
+
+exit 1
