2e482199f38128ec252a9a75d63acc33aee0a146
Alex Bzdel <abzdel25@gmail.com>
2022-10-07 23:14:46 +0000

    working on docker/make compatibility

--- projectContainer/conversion.sh
+++ projectContainer/conversion.sh
@@ -11,10 +11,10 @@ echo "$line" > data.txt
 
 # call python script to download data into current folder
 # could further automate this by changing how the .txt file is read into python script
-python download_csv.py
+python3 download_csv.py
 
 # convert downloaded_data.csv into json format
-python csv_to_json.py
+python3 csv_to_json.py
 echo "csv successfully converted to json. Check downloaded_data.json for final result!"
 
 # exit program with success status
